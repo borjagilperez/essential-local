057524ded3d755f07bfe82751204e06565c89397
borjagilperez <6100691+borjagilperez@users.noreply.github.com>
2021-05-19 10:19:55 +0200

    refactor: apt-get dbeaver-ce

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -19,7 +19,11 @@ select opt in "${options[@]}"; do
             ;;
 
         "Install GUIs")
-            sudo snap install dbeaver-ce
+            #sudo snap install dbeaver-ce
+            wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+            echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+            sudo apt-get update
+            sudo apt-get install -y dbeaver-ce
 
             break
             ;;
