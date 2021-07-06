ebb66c88ad1e658b16141aa6a5bced32c00490d8
borjagilperez <6100691+borjagilperez@users.noreply.github.com>
2021-07-06 10:42:37 +0200

    refactor: sbt installation

--- scripts/base.sh
+++ scripts/base.sh
@@ -50,7 +50,8 @@ select opt in "${options[@]}"; do
             sudo dpkg -i /tmp/scala-2.12.10.deb && sudo apt-get install -y --fix-broken
             scala -version
             # sbt
-            echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+            echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+            echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
             curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
             sudo apt-get update && sudo apt-get install -y sbt
 
